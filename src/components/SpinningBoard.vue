24bb88aa265b7436de6f5b53d9e8146c82b4cedf
phantuan <phantuan87@gmail.com>
2019-03-04 17:54:22 +0700

    Restructure code & impement spin handler

---
+++ src/components/SpinningBoard.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="spinningBoard">
+    <!-- TODO Just for testing data loading. Remove this div -->
+    <!-- <div class="userList">
+      <ul>
+        <li v-for="user in users" v-bind:key="user.fullName">
+          {{ user.fullName }}
+          <button @click="deleteUser(user)">
+            Remove
+          </button>
+          <button @click="getUserById(user['.key'])">
+            GET
+          </button>
+        </li>
+      </ul>
+    </div> -->
+
+    <div class="board">
+      <form @submit.prevent="spinHandler(currentUserId)">
+        <h2>Please input your name ^ ^</h2>
+        <input v-model="currentUserId" placeholder="Your name" class="input">
+        <button type="submit">Spin Spin...</button>
+      </form>
+
+      <div v-if="currentUser">
+        <div v-if="isExistRelationship()">You don't get coffee with anyone</div>
+        <div v-if="!isExistRelationship()">You already have coffee with:</div>
+          <ul>
+            <li v-for="relationUser in currentUser.relationship" v-bind:key="relationUser">
+              {{ relationUser }}
+            </li>
+          </ul>
+      </div>
+
+      <div v-if="suggestedUser">{{viewResultMessage()}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import db from '../firebase';
+
+export default {
+  name: 'SpinningBoard',
+  data () {
+    return {
+      currentUser: null,
+      suggestedUser: null,
+    };
+  },
+  firestore() {
+    return {
+      users: this.getAllUsers(),
+    };
+  },
+  methods: {
+    isExistRelationship() {
+      return this.currentUser && this.currentUser.relationship.length === 0;
+    },
+    spinHandler: function(currentUserId) {
+      this.suggestedUser = null;
+      console.log(currentUserId);
+
+      const currentUser = this.users.find(function(u) {
+        return u['.key'] === currentUserId;
+      });
+
+      if (!currentUser) {
+        alert('Cant find this user !');
+        this.currentUser = null;
+        return;
+      }
+
+      console.log('currentUser', currentUser);
+      console.log('currentUser.relationship', currentUser.relationship);
+
+      this.currentUser = currentUser;
+      this.getSuggestedUser();
+    },
+    viewResultMessage: function() {
+      if (this.suggestedUser.length === 0) {
+        return "You've already caffeinated with everyone!";
+      }
+
+      const numberOfRelationship = this.currentUser.relationship.length;
+      return `Get coffee with: ${this.suggestedUser.fullName}, here’s how many times you’ve had coffee with them: ${numberOfRelationship}`;
+    },
+    getSuggestedUser: function () {
+      if (!this.currentUser) {
+        return null;
+      }
+
+      const currentRelationShip = this.currentUser.relationship;
+      const allUsers = this.users.map(function(u) {
+        return u['.key']
+      });
+
+      console.log('currentRelationShip', currentRelationShip);
+      console.log('allUsers', allUsers);
+
+      let that = this;
+      const listUserSatify = allUsers.filter(function(curUserId) {
+        const index = currentRelationShip.findIndex(function(reUserId) {
+          return reUserId === curUserId;
+        });
+
+        console.log(index);
+        return index === -1 && curUserId !== that.currentUserId;
+      });
+
+      if (listUserSatify.length === 0) {
+        this.suggestedUser = [];
+        return;
+      }
+
+      console.log('listUserSatify', listUserSatify);
+
+      const suggestedUserId = this.randomUser(listUserSatify);
+      const suggestedUser = this.users.find(function(u) {
+        return u['.key'] === suggestedUserId;
+      });
+
+      console.log('suggestedUser', suggestedUser);
+      this.suggestedUser = suggestedUser;
+    },
+    randomUser: function(users) {
+      if (!users) {
+        return null;
+      }
+
+      return users[Math.floor(Math.random() * users.length)];
+    },
+    getAllUsers: function() {
+      return db.collection('users');
+    },
+    getUserById: function(docId) {
+      var docRef = db.collection('users').doc(docId);
+
+      docRef.get().then(function(doc) {
+        if (doc.exists) {
+            console.log('Document data:', doc.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log('No such document!');
+        }
+      }).catch(function(error) {
+        console.log('Error getting document:', error);
+      });
+    },
+    addUser: function() {
+      // TODO Implement this function
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <SpingningBoard></SpingningBoard>
+  </div>
+</template>
+
+<script>
+import SpingningBoard from '@/components/SpinningBoard';
+
+export default {
+  name: 'Home',
+  components: {
+    'SpingningBoard': SpingningBoard
+  }
+};
+</script>
